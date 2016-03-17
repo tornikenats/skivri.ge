c46511d83dd9d43fca292df274f3aa635de4affe
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-03-17 14:42:31 -0400

    add fteching message

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 import data_fetcher
 import time
 import config
+from logger import logging
+from datetime import datetime
 
 while True:
+    logging.info("Start fetching at {0}".format(datetime.utcnow()))
     data_fetcher.update_all_sources()
     time.sleep(config.fetch_wait_secs)
