d99856586ce9766fa5ae1cafe6f86a7db12a6de2
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-03-19 11:58:35 -0400

    add flask logging

--- frontend/app/server.py
+++ frontend/app/server.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import logging
 
 server = Flask(__name__, static_url_path='')
-
+if not server.debug:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    server.logger.addHandler(stream_handler)
+    
 from .blueprints.base import base_api
 server.register_blueprint(base_api, url_prefix='')
