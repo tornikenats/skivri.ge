76e76d345925327935baea236241bffab63181b8
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-03-29 03:11:54 -0400

    updates to frontend

--- frontend/app/blueprints/base.py
+++ frontend/app/blueprints/base.py
@@ -13,13 +13,16 @@ ArticlesTable = articles.initialize(config.settings['MYSQL_DB'], config.settings
 
 
 @base_api.route('/')
-@base_api.route('/index')
-def root():
-    ArticlesTable.connect()
+@base_api.route('/news')
+def news():
+    language = request.args.get('lang', 'eng')
     current_page = int(request.args.get('page', 1))
+    
+    ArticlesTable.connect()
     articles_per_page = 20
-    all_articles = ArticlesTable.select()
+    all_articles = ArticlesTable.select().where(ArticlesTable.lang == language)
     total_pages = math.ceil(all_articles.count() / articles_per_page)
+
     page_boundries = {}
     if current_page < 3:
         page_boundries['min'] = 1
@@ -41,7 +44,7 @@ def root():
 
 
     ArticlesTable.disconnect()
-    return render_template('index.html', articles=articles, current_page=current_page, total_pages=total_pages, page_boundries=page_boundries)
+    return render_template('news.html', articles=articles, current_page=current_page, total_pages=total_pages, page_boundries=page_boundries)
 
 @base_api.app_template_filter()
 def timedelta(pub_date):
@@ -54,19 +57,19 @@ def timedelta(pub_date):
     
     days_str = ''
     if days != 0:
-        days_str = '{} day'.format(days)
+        days_str = '{:0.0f} day'.format(days)
         if days > 1: days_str += 's'
         days_str += ', '
 
     hours_str = ''
     if hours != 0:
-        hours_str = '{} hour'.format(hours)
+        hours_str = '{:0.0f} hour'.format(hours)
         if hours > 1: hours_str += 's'
         hours_str += ', '
 
     minutes_str = ''
     if minutes != 0:
-        minutes_str = '{} minute'.format(minutes)
+        minutes_str = '{:0.0f} minute'.format(minutes)
         if minutes > 1: minutes_str += 's'
         minutes_str += ', '
 
