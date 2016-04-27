1d61bb42d315a16367b37bf0c6c921fce75a256d
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-04-27 14:30:20 -0400

    fix error because create_tables must take a list

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from logger import logging
 
 mydb.init(config.settings['MYSQL_DB'], max_connections=20, stale_timeout=600,
           **{'user': config.settings['MYSQL_USER'], 'password': config.settings['MYSQL_PASS'] })
-mydb.create_tables(Articles, safe=True)
+mydb.create_tables([Articles], safe=True)
 
 while True:
     # execute all scrapers
