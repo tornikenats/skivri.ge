c93a0c4636dbc4bc50a1fb7a4c73bd571fcba655
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-05-06 00:15:10 -0700

    changes in stats

--- frontend/app/blueprints/analytics.py
+++ frontend/app/blueprints/analytics.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import request, abort, jsonify
 from model.analytics import Users, PageViews, mydb
 from playhouse.shortcuts import model_to_dict
-from datetime import datetime
+from datetime import datetime, timedelta
 import config
 
 analytic_api = Blueprint('analytic_api', __name__)
@@ -40,9 +40,66 @@ def report_pageview():
     return '', 204
 
 
+@analytic_api.route('/pageviewshourly', methods=["GET"])
+def page_views_hourly():
+    start_date = request.args.get('start-date')
+    end_date = request.args.get('end-date')
+
+    if not start_date or not end_date:
+        return jsonify(exception="must specify the start-date and end-date")
+
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    if end_date - start_date > timedelta(days=31):
+        return jsonify(exception="time range is too large")
+
+    hourly = {}
+    for view in PageViews.select(PageViews.date, PageViews.user)\
+                        .where(PageViews.date.between(start_date, end_date)):
+        group = view.date.strftime('%B %d, %Y %H:00:00')
+        if not group in hourly:
+            hourly[group] = 0
+
+        hourly[group] += 1
+
+    return jsonify({"hourly_groups": hourly})
+
+
 @analytic_api.route('/pageviews', methods=["GET"])
 def page_views():
-    pageviews = []
-    for view in PageViews.select(PageViews.date):
-        pageviews.append(view.date.strftime('%B %d, %Y %H:%M:%S'))
-    return jsonify({'pageviews': pageviews})
+    start_date = request.args.get('start-date')
+    end_date = request.args.get('end-date')
+
+    if not start_date or not end_date:
+        return jsonify(exception="must specify the start-date and end-date")
+
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    if end_date - start_date > timedelta(days=31):
+        return jsonify(exception="time range is too large")
+
+    anonymized = {}
+    anon_count = 0
+    daily = {}
+    for view in PageViews.select(PageViews.date, PageViews.user)\
+                        .where(PageViews.date.between(start_date, end_date)):
+        # anonymize
+        if not view.user_id in anonymized:
+            anonymized_user_id = anon_count
+            anonymized[view.user_id] = anonymized_user_id
+            anon_count += 1
+        else:
+            anonymized_user_id = anonymized[view.user_id]
+
+        group = view.date.strftime('%B %d, %Y')
+        if not group in daily:
+            daily[group] = {}
+
+        if not anonymized_user_id in daily[group]:
+            daily[group][anonymized_user_id] = 0
+
+        daily[group][anonymized_user_id] += 1
+
+    return jsonify({"daily_groups": daily})
--- frontend/app/static/js/stats.js
+++ frontend/app/static/js/stats.js
@@ -3,44 +3,165 @@ var monthNames = ["January", "February", "March", "April", "May", "June",
 ];
 
 window.onload = function () {
-    var ctx = document.getElementById("pvChart");
-    $.get('/analytics/pageviews')
-        .done(function (resp) {
-            groups = {};
-            resp.pageviews.forEach(function (pageview_date) {
-                date_obj = new Date(pageview_date);
-                group = new Date(date_obj.getFullYear(), date_obj.getMonth(), date_obj.getDate());
-                groups[group] = groups[group] || [];
-                groups[group].push(pageview_date);
-            });
-
-            labels = Object.keys(groups).map(function (group) {
-                date_obj = new Date(group);
-                return monthNames[date_obj.getMonth()] + ' ' + date_obj.getDate();
-            })
-            data = Object.keys(groups).map(function (group) {
-                return groups[group].length;
-            })
-
-            var pvChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Page views',
-                        data: data
-                    }]
+    var dailyChart = c3.generate({
+        bindto: '#dailyChart',
+        types: {
+            Monthly: 'bar'
+        },
+        data: {
+            x: 'labels',
+            columns: [['labels'], ['Monthly']],
+            type: 'bar'
+        },
+        axis: {
+            x: {
+                type: 'category',
+                tick: {
+                    rotate: 75,
+                    multiline: false
                 },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
+                height: 80
+            }
+        },
+        legend: {
+            show: false
+        },
+        tooltip: {
+            grouped: false
+        }
+    })
 
-        });
+
+    var hourlyChart = c3.generate({
+        bindto: '#hourlyChart',
+        data: {
+            x: 'labels',
+            columns: [['labels'], ['Hourly']],
+        },
+        axis: {
+            x: {
+                type: 'category',
+                tick: {
+                    rotate: 75,
+                    multiline: false,
+                    culling: {
+                        max: 7 // the number of tick texts will be adjusted to less than this value
+                    }
+                },
+                height: 100
+            }
+        },
+        legend: {
+            show: false
+        },
+    })
+
+    start_date = new Date()
+    start_date.setDate(start_date.getDate() - 7); // week worth of data
+    end_date = new Date()
+
+    $.get('/analytics/pageviews?start-date=' + formatDate(start_date) + '&end-date=' + formatDate(end_date))
+    .done(function (resp) {
+        daily_groups = resp['daily_groups']
+
+        labels = ['labels']
+        Object.keys(daily_groups).forEach(function(group){
+            date_obj = new Date(group);
+            labels.push(monthNames[date_obj.getMonth()] + ' ' + date_obj.getDate());
+        })
+
+        columns = []
+        // get all users
+        users = []
+        Object.keys(daily_groups).forEach(function(group){
+            Object.keys(daily_groups[group]).forEach(function(str_user_id) {
+                user_id = parseInt(str_user_id)
+                if(users.indexOf(user_id) < 0) {
+                    users.push(parseInt(user_id))
+                    columns[user_id] = []
+                }
+            })
+        })
+
+        Object.keys(daily_groups).forEach(function(group){
+            users_in_group = []
+            // for users in period add their counts
+            Object.keys(daily_groups[group]).forEach(function(str_user_id){
+                user_id = parseInt(str_user_id)
+                columns[user_id].push(daily_groups[group][user_id])
+                users_in_group.push(user_id)
+            })
+            // for all other users add 0
+            users.filter(function(user_id){
+                return users_in_group.indexOf(user_id) < 0
+            })
+            .forEach(function(user_id){
+                columns[user_id].push(0)
+            })
+        })
+
+        for(i = 0; i < columns.length; i++){
+            columns[i].splice(0, 1, "U"+i)
+        }
+
+        dailyChart.groups([columns.map(function(column){return column[0]})])
+        columns.push(labels)
+
+        dailyChart.load({
+            columns: columns
+        })
+    });
+
+    $.get('/analytics/pageviewshourly?start-date=' + formatDate(start_date) + '&end-date=' + formatDate(end_date))
+    .done(function(resp){
+        hourly_groups = resp['hourly_groups']
+
+        columns = []
+        columns[0] = getDates(start_date, end_date).map(function(date_obj){
+            var hours = date_obj.getHours() < 10 ? '0' + date_obj.getHours() : date_obj.getHours();
+            return monthNames[date_obj.getMonth()] + ' ' + date_obj.getDate() + ', ' + date_obj.getFullYear() + " " + hours + ":00:00";
+        })
+
+        columns[1] = []
+        columns[0].map(function(date_str){
+            if(hourly_groups[date_str] != undefined){
+                columns[1].push(hourly_groups[date_str])
+            }else{
+                columns[1].push(0)
+            }
+        })
+
+        columns[0].splice(0,1,'labels')
+        columns[1].splice(0,1,'Views')
+        hourlyChart.load({
+            columns: columns
+        })
+    })
 };
+
+function formatDate(date){
+    // YYYY-mm-dd
+    return date.toISOString().slice(0, 10);
+}
+
+Date.prototype.addDays = function(days) {
+    var dat = new Date(this.valueOf())
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
+
+Date.prototype.addHours = function(hours){
+    var dat = new Date(this.valueOf());
+    dat.setTime(dat.getTime() + (hours*60*60*1000));
+    return dat;
+}
+
+function getDates(startDate, stopDate) {
+    var dateArray = new Array();
+    var currentDate = startDate;
+    while (currentDate <= stopDate) {
+        dateArray.push( new Date (currentDate) )
+        currentDate = currentDate.addHours(1);
+    }
+    return dateArray;
+}
