44cb0f80d3b34aaf58cad4caaaa78ec92b88bb45
tnatsvli <tnatsvli@mail.uoguelph.ca>
2017-07-03 00:42:18 -0400

    remove watch flag

--- application/webpack.config.js
+++ application/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    watch: true,
     entry: './skivrige/src/js/index.js',
     output: {
         path: path.resolve('skivrige/static/js'),
