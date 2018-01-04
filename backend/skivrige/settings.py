3e3b839163a76c71a0eac54d04b19ccded09d0c8
tnatsvli <tnatsvli@mail.uoguelph.ca>
2018-01-03 20:18:26 -0500

    Remove mysql settings

--- backend/skivrige/settings.py
+++ backend/skivrige/settings.py
@@ -4,10 +4,6 @@ class Config(object):
     """Base configuration."""
 
     SECRET_KEY = os.environ.get('APP_SECRET', 'SET IN DOCKER ENV')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'SET IN DOCKER ENV')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'SET IN DOCKER ENV')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'SET IN DOCKER ENV')
-    MYSQL_PASS = os.environ.get('MYSQL_PASS', 'SET IN DOCKER ENV')    
     APP_DIR = os.path.abspath(os.path.dirname(__file__))
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     ARTICLES_PER_PAGE = 20
--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -5,11 +5,6 @@ class Config(object):
     """Base configuration."""
 
     SECRET_KEY = os.environ.get('APP_SECRET', 'CHANGE')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'CHANGE')
-    MYSQL_PORT = os.environ.get('MYSQL_PORT', 'CHANGE')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'CHANGE')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'CHANGE')
-    MYSQL_PASS = os.environ.get('MYSQL_PASS', 'CHANGE')    
     APP_DIR = os.path.abspath(os.path.dirname(__file__))
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     FETCH_WAIT_SECONDS = 60 * 15  # 15 minutes
